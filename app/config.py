b00f9fc354c38367d530aa905f79da92be8f8534
rfsalman <salmanrf2@gmail.com>
2023-06-08 21:52:49 +0700

    feat: mongodb connection

--- app/config.py
+++ app/config.py
@@ -3,12 +3,17 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    MONGODB_URL: str
+    MONGODB_URI: str
     MONGO_INITDB_DATABASE: str
     WEBHOOK_VERIFY_TOKEN: str
     OPEN_AI_API_KEY: str
     FACEBOOK_API_URL: str
     WHATSAPP_ACCESS_TOKEN: str
+    ADMIN_JWT_SECRET: str
+    JWT_ALGORITHM: str
 
     class Config:
         env_file = "./app/.env"
+
+
+settings = Settings()
